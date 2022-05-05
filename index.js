58191443c59f9278c8236cfada97336c71a649ef
SarkerPabon <sarkerpabon@gmail.com>
2022-05-05 12:16:11 +0600

    Add All Products Endpoints

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 
 // Database
 const { connectDB, getDb } = require("./db");
+let db;
 connectDB((err) => {
 	if (!err) {
 		app.listen(port, () => console.log(`Server is running on port ${port}`));
@@ -22,4 +23,16 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
 	res.send("Running Server");
+});
+
+app.get("/products", (req, res) => {
+	let products = [];
+
+	db.collection("products")
+		.find()
+		.forEach((product) => products.push())
+		.then(() => res.status(200).json(products))
+		.catch((error) =>
+			res.status(500).json({ error: "Could not fetch the documents" })
+		);
 });
